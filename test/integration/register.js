ce53f1a9d397c170616e667e334f5b205f8514fc
Ben Drucker <bvdrucker@gmail.com>
2014-01-19 20:50:43 -0500

    Rewrite tests to eliminate repitition

--- test/integration/register.js
+++ test/integration/register.js
@@ -9,23 +9,21 @@ module.exports = function(Bookshelf) {
 
 			beforeEach(function() {
 				Bookshelf._models = {};
-			});
-
-			var Model = Bookshelf.Model.extend({
-				tableName: 'records'
+				this.Model = Bookshelf.Model.extend({
+					tableName: 'records'
+				});
+				this.model = Bookshelf.model('Model', this.Model);
 			});
 
 			it('returns the registered model', function() {
-				equal(Bookshelf.model('Model', Model), Model);
+				equal(this.model, this.Model);
 			});
 
 			it('assigns the model the name', function() {
-				Bookshelf.model('Model', Model);
-				equal(Bookshelf.model('Model'), Model);
+				equal(Bookshelf.model('Model'), this.Model);
 			});
 
 			it('overwrites when there is a name conflict', function() {
-				Bookshelf.model('Model', Model);
 				Bookshelf.model('Model', Bookshelf.Model);
 				equal(Bookshelf.model('Model'), Bookshelf.Model);
 			});
@@ -36,23 +34,21 @@ module.exports = function(Bookshelf) {
 
 			beforeEach(function() {
 				Bookshelf._collections = {};
-			});
-
-			var Collection = Bookshelf.Collection.extend({
-				property: {}
+				this.Collection = Bookshelf.Collection.extend({
+					property: {}
+				});
+				this.collection = Bookshelf.collection('Collection', this.Collection);
 			});
 
 			it('returns the registered collection', function() {
-				equal(Bookshelf.collection('Collection', Collection), Collection);
+				equal(this.collection, this.Collection);
 			});
 
 			it('gives the collection a name', function() {
-				Bookshelf.collection('Collection', Collection);
-				equal(Bookshelf.collection('Collection'), Collection);
+				equal(Bookshelf.collection('Collection'), this.Collection);
 			});
 
 			it('overwrites the collection when there is a name conflict', function() {
-				Bookshelf.collection('Collection', Collection);
 				Bookshelf.collection('Collection', Bookshelf.Collection);
 				equal(Bookshelf.collection('Collection'), Bookshelf.Collection);
 			});
